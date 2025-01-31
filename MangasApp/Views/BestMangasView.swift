7c82f5bd1e88372325bfb4cda41de1ab879b2d73
María Pérez <123765659+mariaperez15@users.noreply.github.com>
2025-01-31 18:35:57 +0100

    add pop up in best mangas

--- MangasApp/Views/BestMangasView.swift
+++ MangasApp/Views/BestMangasView.swift
@@ -9,46 +9,91 @@ import SwiftUI
 
 struct BestMangasView: View {
     @Environment(MangasVM.self) var vm
+    @State private var showPopup = false
+    @State private var selectedManga: MangaModel? = nil
     
     var body: some View {
-        // NavigationStack {
-        ScrollView(.horizontal) {
-            VStack {
-                HStack(spacing: 15) {
-                    ForEach(vm.mangas) {manga in
-                        //NavigationLink(value: manga){
-                        AsyncImage(url: manga.cleanURL) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(maxWidth: .infinity)
-                                .cornerRadius(12)
-                                .frame(width: 160)
-                                .clipped()
-                        } placeholder: {
-                            Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height: 100)
-                                .cornerRadius(12)
+        ZStack {
+            ScrollView(.horizontal) {
+                VStack{
+                    HStack(spacing: 15) {
+                        ForEach(vm.mangas) {manga in
+                            AsyncImage(url: manga.cleanURL) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: .infinity)
+                                    .cornerRadius(12)
+                                    .frame(width: 160)
+                                    .clipped()
+                                    .onTapGesture {
+                                        selectedManga = manga
+                                        withAnimation {
+                                            showPopup = true
+                                        }
+                                    }
+                            } placeholder: {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .cornerRadius(12)
+                            }
                         }
-                        //}
                     }
+                    Spacer()
+                    .task {
+                        await vm.loadBestMangas()
+                    }
+
                 }
-                .task {
-                    await vm.loadBestMangas()
-                }
+                
             }
-            .background(.white)
-            Spacer()
+            if showPopup, let selectedManga = selectedManga {
+                Color.black.opacity(0.1)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        withAnimation {
+                            showPopup = false
+                        }
+                    }
+                
+                VStack {
+                    Text(selectedManga.title)
+                        .font(.title2)
+                        .bold()
+                        .padding()
+                    
+                    AsyncImage(url: selectedManga.cleanURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 150, height: 200)
+                    Text(selectedManga.sypnosis)
+                        .font(.body)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Button("Cerrar") {
+                        withAnimation {
+                            showPopup = false
+                        }
+                    }
+                    .padding()
+                }
+                .frame(width: 300)
+                .background(Color.white)
+                .cornerRadius(15)
+                .shadow(radius: 10)
+                .transition(.scale)
+            }
         }
-        //.navigationTitle("Best mangas")
-        //.navigationDestination(for: MangaModel.self) { manga in
-        //MangaDetailComponent(manga: manga)
-        //}
     }
 }
-//}
 
 
 #Preview {
